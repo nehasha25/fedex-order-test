84fefbe424987839e622476aafb50894efe5c1b4
Neha Sharma <nehasharmaa2009@gmail.com>
2026-02-17 23:44:34 -0500

    new service

---
+++ src/main/java/com/fedex/orders/FedexOrdersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fedex.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FedexOrdersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FedexOrdersServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fedex/orders/FedexOrdersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fedex.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FedexOrdersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
